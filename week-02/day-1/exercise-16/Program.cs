2f3f0b1542cdfa8a52e9a5bbae0e7eb2ba7bd606
Gergo Toth <toothgergo@gmail.com>
2017-09-11 15:35:24 +0200

    exercise-16 done

---
+++ week-02/day-1/exercise-16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two integers
+            // The first represents the number of chickens the farmer has
+            // The seconf represents the number of pigs the farmer has
+            // It should print how many legs all the animals have
+
+            Console.Write("Add meg hany csirkeje van a gazdanak: ");
+            int numberOfChicken = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Add meg hany malaca van a gazdanak: ");
+            int numberOfPigs = Convert.ToInt32(Console.ReadLine());
+
+            int totalLegs = numberOfPigs * 4 + numberOfChicken * 2;
+
+            Console.WriteLine("A gazda allatainak osszesen " + totalLegs + " laba van");
+        }
+
+    }
+}
