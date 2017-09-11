caffefcddd9c2fb20ee8e283dcfbf64945f9c4d3
Gergo Toth <toothgergo@gmail.com>
2017-09-11 16:19:15 +0200

    exercise-19 done

---
+++ week-02/day-1/exercise-19/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number form the standard input,
+            // If the number is zero or smaller it should print: Not enough
+            // If the number is one it should print: One
+            // If the number is two it should print: Two
+            // If the number is more than two it should print: A lot
+
+            Console.WriteLine("Please type a number!");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput <= 0)
+            {
+                Console.WriteLine("Not enough");
+            }
+            else if (userInput == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (userInput == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else
+            {
+                Console.WriteLine("A lot");
+            }
+        }
+    }
+}
