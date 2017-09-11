1f1df6f854b53ad2eeb4abc5a552ae951de541b4
Gergo Toth <toothgergo@gmail.com>
2017-09-11 15:17:44 +0200

    exercise-14 done

---
+++ week-02/day-1/exercise-14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Modify this program to greet user instead of the World!
+            // The program should ask for the name of the user
+            Console.Write("Add meg a neved: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello, " + userName + "!");
+
+        }
+    }
+}
