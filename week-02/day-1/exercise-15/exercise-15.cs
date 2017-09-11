1036674f09cf320118e499a0d22c121b97c0f74e
Gergo Toth <toothgergo@gmail.com>
2017-09-11 15:27:26 +0200

    exercise-15 done

---
+++ week-02/day-1/exercise-15/exercise-15.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for an integer that is a distance in kilometers,
+            // then it converts that value to miles and prints it
+            Console.Write("Adj meg egy tavolsagot km-ben!" );
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Az altalad megadott tavolsag merfoldben :" + userInput * 1.60934);
+        }
+    }
+}
