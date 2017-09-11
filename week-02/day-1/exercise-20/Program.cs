51886fe87f7414fa52dfd040a1b7b41cee9e313b
Gergo Toth <toothgergo@gmail.com>
2017-09-11 16:25:09 +0200

    exercise-20 done

---
+++ week-02/day-1/exercise-20/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that asks for two numbers and prints the bigger one
+            Console.WriteLine("Adj meg egy szamot!");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+			Console.WriteLine("Adj meg egy masik szamot!");
+			int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1 + " a nagyobb");
+            }
+            else if (num1 < num2)
+            {
+                Console.WriteLine(num2 + " a nagyobb");
+            }
+            else
+            {
+                Console.WriteLine("A ket szam egyenlo");
+            }
+        }
+    }
+}
