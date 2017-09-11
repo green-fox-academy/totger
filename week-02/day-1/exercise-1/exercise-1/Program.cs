48c5e4f341375e5eea99edb198fced998341f1fe
Gergo Toth <toothgergo@gmail.com>
2017-09-11 13:23:21 +0200

    first task done

---
+++ week-02/day-1/exercise-1/exercise-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Gergo!");
+        }
+    }
+}
