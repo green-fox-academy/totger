848944f5dbdcfffc553f7d567a8350d18ed84ba9
Gergo Toth <toothgergo@gmail.com>
2017-09-11 14:24:00 +0200

    exercise-10 done

---
+++ week-02/day-1/exercise-10/exercise-10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GreenFox
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            // Define several things as a variable than print their values
+            // Your name as a string
+            // Your age as an integer
+            // Your height in meters as a double
+            // Wether you are married or not as a boolean
+
+            string name = "Gergo";
+            int age = 30;
+            double height = 1.78;
+            bool married = false;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+            Console.WriteLine(height);
+            Console.WriteLine(married);
+		}
+	}
+}
