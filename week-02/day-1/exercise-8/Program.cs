2db833d13448db3dce7c908d2697cf1b726d569d
Gergo Toth <toothgergo@gmail.com>
2017-09-11 14:10:21 +0200

    exercise-8 done

---
+++ week-02/day-1/exercise-8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GreenFox
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Swap the values of the wariables
+			int a = 123;
+			int b = 526;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+		}
+	}
+}
