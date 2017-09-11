3437d46cc130ffbf3ccd11c475efb0ade213e166
Gergo Toth <toothgergo@gmail.com>
2017-09-11 16:09:03 +0200

    exercise-18 done

---
+++ week-02/day-1/exercise-18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that reads a number form the standard input,
+            // Than prints "Odd" if the number is odd, or "Even" it it is even.
+
+            Console.WriteLine("Adj meg egy szamot!");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            if (userInput % 2 == 0){
+                Console.WriteLine("A szam paros");
+            }
+            else {
+                Console.WriteLine("A szam paratlan");
+            }
+
+        }
+    }
+}
