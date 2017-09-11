fbfa7ad3107adb4f2ef244f664796f72c2721e54
Gergo Toth <toothgergo@gmail.com>
2017-09-11 16:01:50 +0200

    exercise-17 done

---
+++ week-02/day-1/exercise-17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace GreenFox
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            // Write a program that asks for 5 integers in a row,
+            // then it should print the sum and the average of these numbers like:
+            //
+            // Sum: 22, Average: 4.4
+
+            Console.WriteLine("Adj meg egy szamot! ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Adj meg egy szamot! ");
+			int num2 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Adj meg egy szamot! ");
+			int num3 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Adj meg egy szamot! ");
+			int num4 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Adj meg egy szamot! ");
+			int num5 = Convert.ToInt32(Console.ReadLine());
+
+            double sum = num1 + num2 + num3 + num4 + num5;
+            double average = sum / 5;
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Average: " + average);
+
+
+		}
+	}
+}
