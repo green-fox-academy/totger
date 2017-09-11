c5adeefae86c1d8179b1dc3b41ce98a1408dcf06
Gergo Toth <toothgergo@gmail.com>
2017-09-11 22:14:50 +0200

    exercise-29 done

---
+++ week-02/day-1/exercise-29/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			// Write a program that reads a number from the standard input, then draws a
+			// pyramid like this:
+			//
+			//
+			//    *
+			//   ***
+			//  *****
+			// *******
+			//
+			// The pyramid should have as many lines as the number was
+
+			Console.WriteLine("Enter a number:");
+
+            int row = Convert.ToInt32(Console.ReadLine());
+			for (int i = 1; i <= row; i++)
+			{
+				//for (int j = i; j <= row; j++)
+				//{
+				//	Console.Write(" ");
+				//}
+				for (int k = 1; k <= i; k++)
+				{
+					Console.Write("* ");
+				}
+				Console.WriteLine();
+			}
+        }
+    }
+}
