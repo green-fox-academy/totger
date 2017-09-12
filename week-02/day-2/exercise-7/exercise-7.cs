29dd39597198d6ecb9a128678f08b0c49e9e0f31
Gergo Toth <toothgergo@gmail.com>
2017-09-12 17:34:22 +0200

    exercise-7 done

---
+++ week-02/day-2/exercise-7/exercise-7.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Third
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            // - Create an array variable named `q`
+            //   with the following content: `[4, 5, 6, 7]`
+            // - Print the third element of `q`
+            int[] q = { 4, 5, 6, 7 };
+            Console.WriteLine(q[2]);
+        }
+	}
+}
