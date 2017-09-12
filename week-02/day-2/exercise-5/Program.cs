4d5785aa3dbd07b06cfcba52378e21b8f55b8bc7
Gergo Toth <toothgergo@gmail.com>
2017-09-12 16:46:24 +0200

    exercise-5 done

---
+++ week-02/day-2/exercise-5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Factorio
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            // - Create a function called `factorio`
+            //   that returns it's input's factorial
+            Console.WriteLine(Factorio(5));
+		}
+
+        public static int Factorio(int input)
+        {
+            for (int i = input; i > 1; i--)
+            {
+                input *= (i - 1);
+            }
+            return input;
+        }
+    }
+}
