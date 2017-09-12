c4949186ff302ea7bd0d4b7b6e25e96e74f1b5a9
Gergo Toth <toothgergo@gmail.com>
2017-09-12 17:25:16 +0200

    exercise-6 done

---
+++ week-02/day-2/exercise-6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Printer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            // - Create a function called `printer`
+            //   which prints the input String parameters
+            // - It can have any number of parameters
+
+            // Examples
+            // printer("first")
+            // printer("first", "second")
+            // printer("first", "second", "third", "fourh")
+            // ...
+            string[] parameter = {"kutya", "lo", "cica", "nyuszi"};
+            Printer(parameter);
+		}
+
+        public static void Printer(string[] parameter)
+        {
+            for (int i = 0; i < parameter.Length; i++)
+            {
+
+                Console.WriteLine(parameter[i]);
+            }
+        }
+    }
+}
