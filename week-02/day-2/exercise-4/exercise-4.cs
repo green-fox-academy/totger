3e1db0108a3836953e52c39faf1004995fdc3806
Gergo Toth <toothgergo@gmail.com>
2017-09-12 16:24:28 +0200

    exercise-4 done

---
+++ week-02/day-2/exercise-4/exercise-4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // - Write a function called `sum` that sum all the numbers
+            //   until the given parameter
+            Console.WriteLine(Sum(5));
+
+        }
+        public static int Sum(int n)
+        {
+            for (int i = n; i > 0; i--)
+            {
+                n += i - 1;
+            }
+            return n;
+        }
+    }
+}
