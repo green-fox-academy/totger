554fe97989788ed6f29da47a7ee5ed60888aeb0c
Gergo Toth <toothgergo@gmail.com>
2017-09-13 14:49:48 +0200

    exercise-9 and 10 done

---
+++ week-02/day-3/exercise-10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElementFinder
+{
+	public class ElementFinder
+	{
+		public static void Main(string[] args)
+		{
+			var List = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
+            // Do this again with a different solution using different list functions!
+            if (List.Contains(7))
+            {
+                Console.WriteLine("Hoorray");
+            }
+            else
+            {
+                Console.WriteLine("Nooooooo");
+            }
+
+  
+        }
+	}
+}
