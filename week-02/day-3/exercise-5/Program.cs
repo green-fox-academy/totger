e9b751bb4f3d245ace78e45f0357bea52e0748fa
Gergo Toth <toothgergo@gmail.com>
2017-09-13 13:33:17 +0200

    exercise-5 done

---
+++ week-02/day-3/exercise-5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reverse
+{
+	public class Reverse
+	{
+		public static void Main(string[] args)
+		{
+			string reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a function that can reverse a String, which is passed as the parameter
+            // Use it on this reversed string to check it!
+            // Try to solve this using ElementAt() first, and optionally anything else after.
+
+
+			
+
+            Console.WriteLine(StringReverser(reversed));
+		}
+
+        private static string StringReverser(string a)
+        {
+            string reverse = "";
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                reverse += a[i];
+            }
+            return reverse;
+
+        }
+    }
+}
