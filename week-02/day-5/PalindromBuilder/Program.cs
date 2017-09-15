897887a0c14628a982986004757093e9ea8f53cc
Gergo Toth <toothgergo@gmail.com>
2017-09-15 13:55:06 +0200

    palindrome builder done

---
+++ week-02/day-5/PalindromBuilder/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PalindromBuilder
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(CreatePalindrom());
+        }
+
+        private static string CreatePalindrom()
+        {
+            string input = GetWord();
+            char[] characters = input.ToCharArray();
+            char[] reversedCharacters = new char[characters.Length];
+            int j = characters.Length - 1;
+            for (int i = 0; i < characters.Length; i++)
+            {
+                reversedCharacters[j] = characters[i];
+                j--;
+            }
+            string reversed = new string(reversedCharacters);
+            return input += reversed;
+        }
+
+        private static string GetWord()
+        {
+            Console.WriteLine("Type in the word to get a palindrom:");
+            return Console.ReadLine();
+        }
+    }
+}
