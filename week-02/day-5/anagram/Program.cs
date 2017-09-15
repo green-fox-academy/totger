b1deba71b84f94a390c026dd67c1636aebe21440
Gergo Toth <toothgergo@gmail.com>
2017-09-15 11:58:14 +0200

    (feat)words input done

---
+++ week-02/day-5/anagram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace anagram
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int count = 1;
+            string word1 = GetWord(count);
+            count++;
+            string word2 = GetWord(count);
+            Console.WriteLine("{0}, {1}", word1, word2);
+
+        }
+
+        private static string GetWord(int count)
+        {
+            Console.WriteLine("Please type in the {0}. word:", count);
+            string word = Console.ReadLine();
+            return word;
+        }
+    }
+}
