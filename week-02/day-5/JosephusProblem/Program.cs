480c628d8ade7e1fce111f3b4e9adcf81571895e
Gergo Toth <toothgergo@gmail.com>
2017-09-15 15:24:24 +0200

    (feat)GuessTheNumber: minor modifications added

---
+++ week-02/day-5/JosephusProblem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace JosephusProblem
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<int> people = new List<int>();
+            int contestant = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < contestant; i++)
+            {
+                people.Add(i + 1);
+            }
+            for (int i = 1; i < people.Count; i += 2)
+            {
+                people[i] = 0;
+            }
+            foreach (var item in people)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
