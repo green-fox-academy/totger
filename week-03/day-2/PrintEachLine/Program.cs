ccaeddbbd3fda61f621984155fe42df10c33d8a0
Gergo Toth <toothgergo@gmail.com>
2017-09-19 11:30:23 +0200

    PrintEachLine done

---
+++ week-03/day-2/PrintEachLine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace PrintEachLine
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Write a program that opens a file called "my-file.txt", then prints
+            // each of lines form the file.
+            // If the program is unable to read the file (for example it does not exists),
+            // then it should print an error message like: "Unable to read file: my-file.txt"
+            string path = "my-file.txt";
+
+            try
+            {
+                string content = File.ReadAllText(path);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The fuck are you doing? try to refer to" +
+                                  "an existing file!");
+            }
+
+		}
+    }
+}
