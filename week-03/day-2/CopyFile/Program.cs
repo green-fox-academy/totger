8af507aaf45e30437426a0106ce3da091ede91e4
Gergo Toth <toothgergo@gmail.com>
2017-09-19 14:02:13 +0200

    CopyFile task finished

---
+++ week-03/day-2/CopyFile/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace CopyFile
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Write a function that reads all lines of a file and writes the read lines to an other file (a.k.a copies the file)
+            // It should take the filenames as parameters
+            // It should return a boolean that shows if the copy was successful
+            string path = "mytext.txt";
+            string outcome = "othertext.txt";
+
+            Console.WriteLine(FileCopy(path, outcome));
+		}
+
+        public static bool FileCopy(string path, string outcome)
+        {
+			string[] content = File.ReadAllLines(path);
+            try
+            {
+               using (StreamWriter writer = new StreamWriter(outcome))
+                {
+                    for (int i = 0; i < content.Length; i++)
+                    {
+                        writer.WriteLine(content[i]);
+                    }
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
