7c1921e8c58925f898f030c49895f28bac6d65b1
Gergo Toth <toothgergo@gmail.com>
2017-09-19 11:54:07 +0200

    CountLines task finished

---
+++ week-03/day-2/CountLines/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace CountLines
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Write a function that takes a filename as string,
+            // then returns the number of lines the file contains.
+            // It should return zero if it can't open the file, and
+            // should not raise any error.
+            string filename = "my-text.txt";
+            Console.WriteLine(LineCounter(filename));
+		}
+
+        private static int LineCounter(string filename)
+        {
+            try
+            {
+                string[] content = File.ReadAllLines(filename);
+                return content.Length;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+    }
+}
