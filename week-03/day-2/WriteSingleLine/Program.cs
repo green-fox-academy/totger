0cb7d5df0628e3f7413484fc5b4bc1c0c6ef3b45
Gergo Toth <toothgergo@gmail.com>
2017-09-19 12:51:25 +0200

    WriteSingleLine task finished

---
+++ week-03/day-2/WriteSingleLine/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace WriteSingleLine
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Open a file called "my-file.txt"
+            // Write your name in it as a single line
+            // If the program is unable to write the file,
+            // then it should print an error message like: "Unable to write file: my-file.txt"
+            string path = "my-file.txt";
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.WriteLine("Gergo Toth");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to write file");
+            }
+        }
+    }
+}
