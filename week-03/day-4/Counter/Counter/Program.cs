1658832546ebc3c107338bc7cb2a143cc05b93b3
Gergo Toth <toothgergo@gmail.com>
2017-09-21 10:14:30 +0200

    recursive counter and adder completed

---
+++ week-03/day-4/Counter/Counter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountDown(10);
+            Console.ReadLine();
+        }
+
+        private static void CountDown(int n)
+        {
+            if (n < 0)
+                return;
+            Console.WriteLine(n);
+            CountDown(n - 1);
+        }
+    }
+}
---
+++ week-03/day-4/NumberAdder/NumberAdder/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumberAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AddNumber(6));
+            Console.ReadLine();
+        }
+
+        private static int AddNumber(int n)
+        {
+            if (n == 0) return 0;
+            n += AddNumber(n - 1);
+            return n;
+        }
+    }
+}
