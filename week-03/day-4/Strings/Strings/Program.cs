bb3e1bb5c3bf2ea37a5cd01ce0c968b178e978cd
Gergo Toth <toothgergo@gmail.com>
2017-09-21 15:45:59 +0200

    Strings task done

---
+++ week-03/day-4/Strings/Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static string output;
+        static void Main(string[] args)
+        {
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.
+            Console.WriteLine(StringChanger("dselfmdsfxXxylkmefyx"));
+            Console.ReadLine();
+        }
+
+        public static string StringChanger(string input)
+        {
+
+            if (input.Length == 0) return output;
+            if (input[0] == 'x')
+            {
+                output += 'y';
+            }
+            else
+            {
+                output += input[0]; 
+            }
+            return StringChanger(input.Substring(1));
+        }
+    }
+}
