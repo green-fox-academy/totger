1d69389d1791af11997120e2dae900a0bacc38d8
Gergo Toth <toothgergo@gmail.com>
2017-09-21 16:04:08 +0200

    Fibonacci task done

---
+++ week-03/day-4/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine(GetFibonacci(8));
+            Console.ReadLine();
+        }
+
+        private static int GetFibonacci(int n)
+        {
+            if (n <= 1) return 0;
+            if (n == 2) return 1;
+         
+            return GetFibonacci(n - 1) + GetFibonacci(n - 2);   
+        }
+    }
+}
