7de381ac0b446859c3246fa4a765b141b594bcc2
Gergo Toth <toothgergo@gmail.com>
2017-09-21 10:51:35 +0200

    SumDigit task completed

---
+++ week-03/day-4/SumDigit/SumDigit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given a non-negative int n, return the sum of its digits recursively (no loops). 
+            // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
+            // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+            Console.WriteLine(Digits(12614));
+            Console.ReadLine();
+        }
+
+        private static int Digits(int n)
+        {
+            if (n <= 0) return 0;
+            n = n % 10 + Digits(n / 10);
+            return n;
+        }
+    }
+}
