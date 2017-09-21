9304bfef60baabb601aa5cf6c6bef3652ea1a674
Gergo Toth <toothgergo@gmail.com>
2017-09-21 11:54:23 +0200

    Bunnies task completed

---
+++ week-03/day-4/Bunnies/Bunnies/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Bunnies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).           
+            Console.WriteLine(BunnyEars(4));
+            Console.ReadLine();
+        }
+
+        private static int BunnyEars(int number)
+        {
+            if (number == 0) return 0;
+            return 2 + BunnyEars(number - 1);
+        }
+    }
+}
---
+++ week-03/day-4/Power/Power/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Given base and n that are both 1 or more, compute recursively (no loops)
+            // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+            Console.WriteLine(PowerN(2, 8));
+            Console.ReadLine();
+        }
+
+        private static int PowerN(int baseNum, int power)
+        {
+            if (baseNum < 1 || power < 1) return 1;
+            baseNum *= PowerN(baseNum, power - 1);
+            return baseNum; 
+        }
+    }
+}
