d01daab4fc34f8fad5aadc3c16e1d55d3dac089d
Gergo Toth <toothgergo@gmail.com>
2017-09-25 13:51:37 +0200

    blog task done

---
+++ week-04/day-1/Blog/BlogPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Blog
+{
+    public class BlogPost
+    {
+        public string AuthorName;
+        public string Title;
+        public string Text;
+        public string PublicationDate;
+        public BlogPost()
+        {
+        }
+    }
+}
---
+++ week-04/day-1/Blog/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Blog
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            BlogPost post1 = new BlogPost();
+            BlogPost post2 = new BlogPost();
+            BlogPost post3 = new BlogPost();
+
+            post1.AuthorName = "John Doe";
+            post1.Title = "Lorem Ipsum";
+            post1.PublicationDate = "2000.05.04";
+            post1.Text = "Lorem ipsum dolor sit amet.";
+
+            post2.AuthorName = "Tim Urban";
+            post2.Title = "Wait but why";
+            post2.PublicationDate = "2010.10.10";
+            post2.Text = "A popular long-form, stick-figure-illustrated blog " +
+                         "about almost everything.";
+
+            post3.AuthorName = "William Turton";
+            post3.Title = "One Engineer Is Trying to Get IBM to Reckon With Trump";
+            post3.PublicationDate = "2017.03.28";
+            post3.Text = "Daniel Hanley, a cybersecurity engineer at IBM, " +
+                         "doesn’t want to be the center of attention. When I " +
+                         "asked to take his picture outside one of IBM’s " +
+                         "New York City offices, he told me that he wasn’t " +
+                         "really into the whole organizer profile thing.";
+        }
+    }
+}
