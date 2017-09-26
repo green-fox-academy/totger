87b28c123f13dd6d5a5c8546c7c7649faa2a749a
Gergo Toth <toothgergo@gmail.com>
2017-09-26 17:40:35 +0200

    feat(GreenFox) pallidaclass class added

---
+++ week-04/day-2/GreenFox/PallidaClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GreenFox
+{
+    public class PallidaClass
+    {
+        private string className;
+        private List<Student> students; new List<Student>();
+        private List<Mentor> mentors; new List<Mentor>();
+
+        public PallidaClass(string ClassName)
+        {
+            className = ClassName;
+            students = new List<Student>();
+            mentors = new List<Mentor>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void AddMentor(Mentor mentor)
+        {
+            mentors.Add(mentor);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine("Pallida {0} class has {1} students and {2} mentors." ,
+                              className, students.Count, mentors.Count);
+        }
+    }
+}
