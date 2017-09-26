dfe9740873ab9ff2f3c3f057842eba1ca8501439
Gergo Toth <toothgergo@gmail.com>
2017-09-26 17:18:52 +0200

    feat(GreenFox) sponsor class added

---
+++ week-04/day-2/GreenFox/Sponsor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace GreenFox
+{
+    public class Sponsor : Person
+    {
+        private string company;
+        private int hiredStudents;
+
+        public Sponsor(string Name, int Age, string Gender, string Company)
+        {
+			name = Name;
+			age = Age;
+			gender = Gender;
+            company = Company;
+        }
+
+        public Sponsor()
+        {
+			name = "Jane Doe";
+			age = 30;
+			gender = "female";
+            company = "Google";
+            hiredStudents = 0;
+        }
+
+        public new void Introduce()
+        {
+            Console.WriteLine("Hi, I'm {0}, a {1} year old {2} who represents" +
+                              " {3} and hired {4} students so far.", 
+                              name, age, gender, company, hiredStudents);
+        }
+
+        public new void GetGoal()
+        {
+            Console.WriteLine("Hire brilliant junior software developers.");
+        }
+
+        public void Hire()
+        {
+            hiredStudents += 1;
+        }
+    }
+}
