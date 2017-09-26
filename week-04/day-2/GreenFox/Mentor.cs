53f2b909984ebc3f1ae187b36c902499b2910bff
Gergo Toth <toothgergo@gmail.com>
2017-09-26 17:03:11 +0200

    feat(GreenFox) mentor class added

---
+++ week-04/day-2/GreenFox/Mentor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace GreenFox
+{
+    public class Mentor : Person
+    {
+        private string level;
+
+        public Mentor(string Name, int Age, string Gender, string Level)
+        {
+			name = Name;
+			age = Age;
+			gender = Gender;
+            level = Level;
+        }
+
+        public Mentor()
+        {
+			name = "Jane Doe";
+			age = 30;
+			gender = "female";
+            level = "intermediate";
+        }
+
+        public new void GetGoal()
+        {
+            Console.WriteLine("Educate brilliant junior software developers.");
+        }
+
+        public new void Introduce()
+        {
+            Console.WriteLine("Hi, I'm {0}, a {1} year old {2} {3} mentor.", 
+                             name, age, gender, level);
+        }
+    }
+}
