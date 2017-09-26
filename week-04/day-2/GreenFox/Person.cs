7de5dba9178e76391a3f95099b4090f38dc62415
Gergo Toth <toothgergo@gmail.com>
2017-09-26 16:23:28 +0200

    feat(GreenFox) person class added

---
+++ week-04/day-2/GreenFox/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace GreenFox
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        public Person(string Name, int Age, string Gender)
+        {
+            name = Name;
+            age = Age;
+            gender = Gender;
+        }
+
+        public Person()
+        {
+            name = "Jane Doe";
+            age = 30;
+            gender = "female";
+           
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine("Hi, I'm {0}, a {1} age year old {2}", name, age, gender);
+        }
+
+        public void GetGoal()
+        {
+            Console.WriteLine("My goal is: Live for the moment!");
+        }
+
+    }
+}
---
+++ week-04/day-2/GreenFox/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreenFox
+{
+    class Apple
+    {}
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person("Jolan", 49, "female");
+
+        }
+    }
+}
