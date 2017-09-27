9176eac062e3b37268e77735dcacfc327acb2229
Gergo Toth <toothgergo@gmail.com>
2017-09-27 15:35:20 +0200

    anagram test done

---
+++ week-04/day-3/Anagram/Anagramma.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Anagram
+{
+    public class Anagramma
+    {
+        public string Word1;
+        public string Word2;
+
+        public bool IsAnagram(string Word1, string Word2)
+        {
+            char[] char1 = Word1.ToCharArray();
+            char[] char2 = Word2.ToCharArray();
+
+            Array.Sort(char1);
+            Array.Sort(char2);
+
+            Word1 = String.Concat(char1);
+            Word2 = String.Concat(char2);
+
+            if (Word1 == Word2) return true;
+
+            return false;
+        }
+    }
+}
---
+++ week-04/day-3/Anagram/Anagramtest/Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using Anagram;
+
+namespace AnagramTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Anagramma anagram = new Anagramma();
+
+            Assert.IsTrue(anagram.IsAnagram("kakao", "okaka"));
+        }
+    }
+}
