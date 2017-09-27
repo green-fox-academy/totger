dbf11d99f8163714198f66f239c5be5fcd7cb098
Gergo Toth <toothgergo@gmail.com>
2017-09-27 16:26:53 +0200

    letter counter test done and tested

---
+++ week-04/day-3/CountLetters/LetterCounterTest/Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using CountLetters;
+
+namespace LetterCounterTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Letters letters = new Letters();
+            Dictionary<char, int> output = new Dictionary<char, int>(){
+                {'a', 2},
+                {'l', 1},
+                {'m', 1}
+            };
+
+            Assert.AreEqual(output, letters.LetterCounter("alma"));
+        }
+    }
+}
---
+++ week-04/day-3/CountLetters/Letters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CountLetters
+{
+    public class Letters
+    {
+		public Dictionary<char, int> LetterCounter(string input)
+		{
+            Dictionary<char, int> output = new Dictionary<char, int>();
+
+            foreach (char letter in input)
+            {
+                if (output.ContainsKey(letter))
+                {
+                    output[letter]++;
+                }
+                else
+                {
+                    output.Add(letter, 1);
+                }
+            }
+            return output;
+        }
+    }
+}
