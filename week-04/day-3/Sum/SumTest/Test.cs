40cc557f05f1c32343bba23d10c7702836b8c089
Gergo Toth <toothgergo@gmail.com>
2017-09-27 14:55:30 +0200

    sum test done and tested

---
+++ week-04/day-3/Sum/SumTest/Test.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Sum;
+
+namespace SumTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            int sum = 8;
+            var sumNums = new SumClass();
+            List<int> list = new List<int>() {1, 3, 4};
+
+            int output = sumNums.GetSum(list);
+
+            Assert.AreEqual(sum, output);
+        }
+    }
+}
---
+++ week-04/day-3/Sum/Sumclass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sum
+{
+    public class SumClass
+    {
+        List<int> list = new List<int>();
+        public int GetSum(List<int> list)
+        {
+            int sum = 0;
+            foreach (int num in list)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+    }
+}
