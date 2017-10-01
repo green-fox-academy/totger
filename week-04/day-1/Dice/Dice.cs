3e41dbce4ca215b32b8f4c3d165ca1bd0625231d
Gergo Toth <toothgergo@gmail.com>
2017-10-01 09:17:49 +0200

    refact(Dice)

---
+++ week-04/day-1/Dice/Dice.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DiceSimulation
+{
+    public class Dice
+    {
+        static Random RandomValue = new Random();
+
+        public List<int> dice = new List<int>();
+
+        public Dice(int diceNum)
+        {
+            for (int i = 0; i < diceNum; i++)
+            {
+                dice.Add(RandomValue.Next(1, 7));
+            }
+        }
+
+        public void RollAll()
+        {
+            for (int i = 0; i < dice.Count; i++)
+            {
+                dice[i] = RandomValue.Next(1, 7);
+            }
+        }
+
+        public void RollOne(int i)
+        {
+            dice[i] = RandomValue.Next(1, 7);
+        }
+
+		public void RollAllUntilAllSixes()
+		{
+            while (!AreOnlySixes())		
+            {
+                RollAll();
+			}
+		}
+         
+		public bool AreOnlySixes()
+		{
+			foreach (int item in dice)
+			{
+				if (item != 6)
+					return false;
+			}
+			return true;
+		}
+
+        public void OneByOneRecursive(int n)
+        {
+            while (dice[n] != 6)
+            {
+                RollOne(n);
+            }
+            if (n != 0)
+                OneByOneRecursive(n - 1);
+        }
+
+		public void OneByOne()
+		{
+            for (int i = 0; i < dice.Count; i++)
+			{
+				while (dice[i] != 6)
+				{
+                    RollOne(i);
+				}
+			}
+		}
+		
+        public void RerollExceptSixes()
+		{
+			List<int> reRoll = CreateRerollList();
+            while (reRoll.Count > 0)
+            {
+				foreach (int index in reRoll)
+				{
+                    RollOne(index);
+				}
+                UpdateList(reRoll);
+            }
+		}
+
+        public List<int> CreateRerollList()
+		{
+            List<int> reRoll = new List<int>();
+            for (int i = 0; i < dice.Count; i++)
+            {
+                if (dice[i] != 6) 
+                    reRoll.Add(i);
+            }
+            return reRoll;
+		}
+
+        public void UpdateList (List<int> reRoll)
+        {
+            for (int i = 0; i < reRoll.Count; i++)
+            {
+                if (dice[reRoll[i]] == 6)
+				{
+                    reRoll.RemoveAt(i);
+				}        
+            }
+        }
+    }
+}
---
+++ week-04/day-1/Dice/Simulation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+namespace DiceSimulation
+{
+    public class Simulation
+    {
+        static Stopwatch stopWatch = new Stopwatch();
+        private int times;
+        		
+        public void StartSimulation(int diceNum, int times)
+		{
+            var testDice = SetupSimulation(diceNum, times);
+			
+            for (int method = 2; method <= 4; method++)
+			{
+				for (int j = 0; j < times; j++)
+				{
+					RunSimulation(testDice, method);
+				}
+				PrintResults(method, times);
+				stopWatch.Reset();
+			}
+		}
+
+        public Dice SetupSimulation(int diceNum, int times)
+        {
+			this.times = times;
+            Dice testDice = new Dice(diceNum);
+
+			Console.WriteLine("*** {0} Dice *** {1} Runs ***",
+							  testDice.dice.Count, times);
+            return testDice;
+        }
+
+        public void RunSimulation(Dice testDice, int method)
+		{
+            testDice.RollAll();
+			switch (method)
+			{
+				case 1:
+					stopWatch.Start();
+					testDice.RollAllUntilAllSixes();
+					stopWatch.Stop();
+					break;
+				case 2:
+                    stopWatch.Start();
+                    testDice.OneByOneRecursive(testDice.dice.Count - 1);
+					stopWatch.Stop();
+					break;
+				case 3:
+					stopWatch.Start();
+					testDice.OneByOne();
+					stopWatch.Stop();
+					break;
+				case 4:
+					stopWatch.Start();
+					testDice.RerollExceptSixes();
+					stopWatch.Stop();
+					break;
+			}
+		}
+
+		public void PrintResults(int method, int times)
+		{
+			TimeSpan timespan = stopWatch.Elapsed;
+			double result = timespan.TotalMilliseconds;
+			Console.WriteLine("\n{0}. method\n" +
+							  "Average time: {1} ms\n" +
+                              "_______________________________",
+							  method, result / times);
+		}
+	}
+}
