f641b742319153ee6318eef6ac95393287a6e60f
Gergo Toth <toothgergo@gmail.com>
2017-10-18 11:07:06 +0200

    feat(EvenNumbers) query method added

---
+++ week-06/day-3/EvenNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenNumbers
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<int> evenNumbers = GetEvenNumbersQuery();
+        }
+
+        private static List<int> GetEvenNumbersQuery()
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var evenNumbers =
+                from number in n
+                where number % 2 == 0
+                select number;
+
+            return evenNumbers.ToList<int>();
+        }
+    }
+}
