fed5cd8963d73e37c75b882686416e0b2d4efa8e
Gergo Toth <toothgergo@gmail.com>
2017-10-18 17:41:33 +0200

    feat(FoxLinq): finding green and pallida fox with lambda expression

---
+++ FoxLinq/Fox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace FoxLinq
+{
+    public class Fox
+    {
+        public string Name;
+        public string Type;
+        public string Color;
+
+        public Fox(string n, string t, string c)
+        {
+            Name = n;
+            Type = t;
+            Color = c;
+        }
+    }
+}
---
+++ FoxLinq/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FoxLinq
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<Fox> Foxlist = new List<Fox>
+            {
+                new Fox("bela", "pallida", "green"),
+                new Fox("helga", "arctic", "white"),
+                new Fox("lolka", "spacefox", "purple"),
+                new Fox("abel", "oceanfox", "green"),
+                new Fox("majk", "forestfox", "red")
+            };
+
+            var output = FindGreenFox(Foxlist);
+
+            var output2 = FindGreenPallida(Foxlist);
+        }
+
+        public static List<Fox> FindGreenPallida(List<Fox> list)
+        {
+            return list.Where(x => x.Color == "green" && x.Type == "pallida").Select(x => x).ToList();
+        }
+
+        public static List<Fox> FindGreenFox(List<Fox> list)
+        {
+            return list.Where( x => x.Color == "green").Select(x => x).ToList();
+        }
+    }
+}
