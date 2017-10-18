e05c5cbe337f7e022d6fb58a4691212c02d58e20
Gergo Toth <toothgergo@gmail.com>
2017-10-18 13:57:07 +0200

    feat(Frequency): lambda expression added

---
+++ week-06/day-3/Frequency/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Frequency
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] n = { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+
+            var output = GetFrequencyLambda(n);
+        }
+
+        private static Dictionary<int, int> GetFrequencyLambda(int[] n)
+        {
+            Dictionary<int, int> output = new Dictionary<int, int>();
+            var uniqueNumbers = n.Distinct();
+
+            foreach (var number in uniqueNumbers)
+            {
+                output.Add(number, n.Count(x => x == number));
+            }
+
+            return output;
+        }
+    }
+}
