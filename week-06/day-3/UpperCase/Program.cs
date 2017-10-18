e69c23a0af9db15b473035676afa2c59f5b39c33
Gergo Toth <toothgergo@gmail.com>
2017-10-18 16:19:34 +0200

    feat(UpperCase): query method added

---
+++ week-06/day-3/UpperCase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UpperCase
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string input = "Nem AkaRom Megint A sucCulentset";
+
+            var output = GetUpperLettersQuery(input);
+        }
+
+        private static List<char> GetUpperLettersQuery(string input)
+        {
+
+            var output =
+                (from letter in input
+                 where char.IsUpper(letter)
+                 select letter).ToList();
+
+            return output;
+        }
+    }
+}
