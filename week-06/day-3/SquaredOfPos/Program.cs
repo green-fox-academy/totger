3d6eb33e3659514efc4b43d116f097ac40e84b8d
Gergo Toth <toothgergo@gmail.com>
2017-10-18 11:59:14 +0200

    feat(SquareOfPos): lambda expression and query method added

---
+++ week-06/day-3/SquaredOfPos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace SquaredOfPos
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var squares = GetSquareLambda(n);
+            var squaresQuery = GetSquareQuery(n);
+        }
+
+        private static object GetSquareQuery(int[] n)
+        {
+            return
+                from number in n
+                where number >= 0
+                select number * number;
+        }
+
+        private static object GetSquareLambda(int[] n)
+        {
+            return n.Where(x => x >= 0).Select(x => x * x);
+        }
+    }
+}
