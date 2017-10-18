a3dea05c4eefaa64ee8d30a341711b8366a7d920
Gergo Toth <toothgergo@gmail.com>
2017-10-18 16:02:28 +0200

    feat(StartAndEnd):lambda expression and  query method added

---
+++ week-06/day-3/StartAndEnd/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+            
+
+namespace StartAndEnd
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] cities = { "ROME", "LONDON", "NAIROBI", "CALIFORNIA", "ZURICH", "NEW DELHI", "AMSTERDAM", "ABU DHABI", "PARIS" };
+
+            var output = GetCitiesLambda(cities);
+            var outputQuery = GetCitiesQuery(cities);
+        }
+
+        private static object GetCitiesQuery(string[] cities)
+        {
+            return
+                from city in cities
+                where city.StartsWith("A") && city.EndsWith("I")
+                select city;
+        }
+
+        private static object GetCitiesLambda(string[] cities)
+        {
+            return cities.Where(x => x.StartsWith("A") && x.EndsWith("I"));
+        }
+    }
+}
