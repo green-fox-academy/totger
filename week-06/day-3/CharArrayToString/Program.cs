fc31e593a79434e104d241590b7f3fe272a8fb2f
Gergo Toth <toothgergo@gmail.com>
2017-10-18 16:43:38 +0200

    feat(CharArrayToString): done

---
+++ week-06/day-3/CharArrayToString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace CharArrayToString
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            char[] input = { 'a', 'o', 'k', 'p', 'x' };
+            string output = ToString(input);
+        }
+
+        private static string ToString(char[] input)
+        {
+            return String.Join("", input.Select(x => x));
+        }
+    }
+}
