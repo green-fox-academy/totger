d1b6ee654a1a8fc49c4d15214e2523ef14e55df2
Gergo Toth <toothgergo@gmail.com>
2017-10-18 11:26:35 +0200

    feat(OddsAverage): lambda expression added

---
+++ week-06/day-3/AverageOfOdds/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageOfOdds
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var output = GetAverageOfOddLambda();
+        }
+
+        private static double GetAverageOfOddLambda()
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            var average = n.Where(x => x % 2 != 0).Average();
+
+            return average;
+        }
+    }
+}
