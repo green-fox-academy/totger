1b4b9430d5e5aeeb988d6c2d907b150b43b87644
Gergo Toth <toothgergo@gmail.com>
2017-10-18 11:11:03 +0200

    feat(EvenNumbers) querlambda expression added

--- week-06/day-3/EvenNumbers/Program.cs
+++ week-06/day-3/EvenNumbers/Program.cs
@@ -9,6 +9,15 @@ namespace EvenNumbers
         public static void Main(string[] args)
         {
             List<int> evenNumbers = GetEvenNumbersQuery();
+            List<int> evenNumbersLambda = GetEvenNumbersLambda();
+        }
+
+        private static List<int> GetEvenNumbersLambda()
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+
+            var evenNumbers = n.Where(x => x % 2 == 0).ToList();
+            return evenNumbers;
         }
 
         private static List<int> GetEvenNumbersQuery()
@@ -20,7 +29,7 @@ namespace EvenNumbers
                 where number % 2 == 0
                 select number;
 
-            return evenNumbers.ToList<int>();
+            return evenNumbers.ToList();
         }
     }
 }
