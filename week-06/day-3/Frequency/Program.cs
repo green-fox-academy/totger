ae72cd49a8bf924243141fa3cad98c2f934eac7d
Gergo Toth <toothgergo@gmail.com>
2017-10-18 14:48:52 +0200

    feat(Frequency): query method added

--- week-06/day-3/Frequency/Program.cs
+++ week-06/day-3/Frequency/Program.cs
@@ -11,18 +11,27 @@ namespace Frequency
             int[] n = { 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
 
             var output = GetFrequencyLambda(n);
+            var outputQuery = GetFrequencyQuery(n);
+        }
+
+        private static Dictionary<int, int> GetFrequencyQuery(int[] n)
+        {
+            return
+                (from number in n
+                group number by number into o
+                select new { o.Key, Value = o.Count() })
+                .ToDictionary(x => x.Key, x => x.Value);
         }
 
         private static Dictionary<int, int> GetFrequencyLambda(int[] n)
         {
             Dictionary<int, int> output = new Dictionary<int, int>();
-            var uniqueNumbers = n.Distinct();
 
-            foreach (var number in uniqueNumbers)
+            foreach (var number in n.Distinct())
             {
                 output.Add(number, n.Count(x => x == number));
             }
-
+           
             return output;
         }
     }
