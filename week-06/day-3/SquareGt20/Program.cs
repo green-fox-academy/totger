7f02980284de412941b6b8083461b297ce743e59
Gergo Toth <toothgergo@gmail.com>
2017-10-18 13:31:46 +0200

    feat(SquareGt20): lamda expression added

---
+++ week-06/day-3/SquareGt20/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SquareGt20
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] n = new[] { 3, 9, 2, 8, 6, 5 };
+            var output = GetSquare(n);
+        }
+
+        private static object GetSquare(int[] n)
+        {
+            return n.Where(x => x * x > 20);
+        }
+    }
+}
