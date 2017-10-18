944e72631995a37630e7219840a785253b383b2e
Gergo Toth <toothgergo@gmail.com>
2017-10-18 15:28:42 +0200

    feat(CharacterFrequency): lambda expression added

---
+++ week-06/day-3/CharacterFrequency/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CharacterFrequency
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string input = "hello world";
+
+            var output = GetCharFreqLambda(input);
+        }
+
+        private static Dictionary<char, int> GetCharFreqLambda(string input)
+        {
+            Dictionary<char, int> output = new Dictionary<char, int>();
+
+            foreach (var character in input
+                     .Where(character => !char.IsWhiteSpace(character))
+                     .Distinct())
+            {
+                output.Add(character, input.Count( x => x == character));
+            }
+
+            return output;
+        }
+    }
+}
