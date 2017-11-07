5c2b8c35300a5da471e9d3b73c9df1e6add3fff2
Gergo Toth <toothgergo@gmail.com>
2017-11-07 14:14:56 +0100

    feat(APIworkshop): greeter endpoint done

--- week-09/day-2/APIworkshop/Controllers/HomeController.cs
+++ week-09/day-2/APIworkshop/Controllers/HomeController.cs
@@ -29,5 +29,21 @@ namespace APIworkshop.Controllers
 			
 			return Json(new { received = input, result = input * 2 });
         }
+        [HttpGet]
+        [Route("greeter")]
+        public IActionResult Greeter(string name, string title)
+        {
+            if (name == null)
+            {
+                return Json(new { error = "Please provide a name!" });
+            }
+
+            if (title == null)
+            {
+                return Json(new { error = "Please provide a title!" });
+            }
+
+            return Json(new { welcome_message = $"Oh, hi there {name}, my dear {title}!" });
+        }
     }
 }
