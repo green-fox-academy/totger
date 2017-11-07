c83d30fa561de9ea8ab07969cb5ed591eeef82f5
Gergo Toth <toothgergo@gmail.com>
2017-11-07 15:58:47 +0100

    feat(APIworkshop): dountil endpoint done

--- week-09/day-2/APIworkshop/Controllers/HomeController.cs
+++ week-09/day-2/APIworkshop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Threading.Tasks;
+using APIworkshop.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -58,6 +59,26 @@ namespace APIworkshop.Controllers
         public IActionResult Append()
         {
             return NotFound();
+        }
+
+        [HttpPost]
+        [Route("/dountil/")]
+        [Route("/dountil/{what}")]
+        public IActionResult DoUntil([FromBody] JsonObject jsonObject, string what)
+        {
+            if (what == "factor")
+            {
+                int factorial = jsonObject.Factorial(jsonObject.Until);
+                return Json(new { result = factorial });
+            }
+
+            if (what == "sum")
+            {
+                int sum = jsonObject.Sum(jsonObject.Until);
+                return Json(new { result = sum });
+            }
+
+            return Json(new { error = "Please provide a number!" });
         }
     }
 }
---
+++ week-09/day-2/APIworkshop/Models/JsonObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace APIworkshop.Models
+{
+    public class JsonObject
+    {
+        public int Until { get; set; }
+
+        public int Factorial(int Until)
+        {
+            if (Until == 1) return 1;
+
+            return Until * Factorial(Until - 1);
+        }
+
+        public int Sum(int Until)
+        {
+            int result = 0;
+            for (int i = 1; i <= Until; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+    }
+}
