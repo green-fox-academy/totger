7b368376a9337a1d493ea22a17e7be1b12cf2d99
Gergo Toth <toothgergo@gmail.com>
2018-01-15 09:28:32 +0100

    feat(final exam): create matrix sum method

---
+++ final exam/MatrixSum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MatrixSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> matrix = new List<List<int>>()
+            {
+                new List<int>() { 1, 2, 3},
+                new List<int>() { 2, 3, 4},
+                new List<int>() { 1, 1, 1}
+            };
+            Console.WriteLine(SumLists(matrix));
+        }
+
+        public static int SumLists(List<List<int>> matrix)
+        {
+            return matrix.Select(x => x.Sum()).Sum();
+        }
+    }
+}
